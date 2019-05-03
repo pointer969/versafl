2d4bfc3833843da42f2dbc8f8d73a7dcd632a82f
marcelo_s_almeida <marcelo_s_almeida@hotmail.com>
2019-05-02 22:23:24 -0400

    adjust layout

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
+  sourceMap: false
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
