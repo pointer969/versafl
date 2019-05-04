9df94b340b2510c6c9742be24e72ae0eeaebb88f
marcelo_s_almeida <marcelo_s_almeida@hotmail.com>
2019-05-04 05:27:59 -0400

    fix css

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var sassMiddleware = require('node-sass-middleware');
+// var sassMiddleware = require('node-sass-middleware');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,13 +18,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: false, // true = .sass and false = .scss
-  debug: true,
-  outputStyle: 'compressed'
-}));
+// app.use(sassMiddleware({
+//   src: path.join(__dirname, 'public'),
+//   dest: path.join(__dirname, 'public'),
+//   indentedSyntax: false, // true = .sass and false = .scss
+//   debug: true,
+//   outputStyle: 'compressed'
+// }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
